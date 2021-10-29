28d147cc0ed179cc3ccc8d86c1575aa08452dc34
Johanna De La Torre <jd.34736.jdlt@gmail.com>
2021-10-29 16:02:19 -0400

    Equaled 'newStory' to story.textContent

--- main.js
+++ main.js
@@ -17,31 +17,30 @@ const insertZ = [
   "turned into a slug and crawled away"
 ];
 
-randomize.addEventListener('click', result);
+randomize.addEventListener("click", result);
 
 function result() {
-let newStory = storyText; 
-let xItem = randomValueFromArray(insertX);
-let yItem = randomValueFromArray(insertY);
-let zItem = randomValueFromArray(insertZ);
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replace(':insertx:',xItem);
-newStory = newStory.replace(':inserty:',yItem);
-newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
 
-  if(customName.value !== '') {
+  if (customName.value !== "") {
     let name = customName.value;
-    newStory = newStory.replace('Bob',name)
+    newStory = newStory.replace("Bob", name);
   }
 
-  if(document.getElementById("uk").checked) {
-    let weight = Math.round(300*0.0714286) + ' stone';
-    let temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-      newStory = newStory.replace('94 fahrenheit',temperature);
-      newStory = newStory.replace('300 pounds',weight);
-
+  if (document.getElementById("uk").checked) {
+    let weight = Math.round(300 * 0.0714286) + " stone";
+    let temperature = Math.round(((94 - 32) * 5) / 9) + " centigrade";
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
   }
 
-  story.textContent = ;
-  story.style.visibility = 'visible';
+  story.textContent = newStory;
+  story.style.visibility = "visible";
 }
